ef7bfd011c9f78acb1d2de549f3839dd1ca6ccf8
DELL <cristian_uteq@hotmail.com>
2025-02-22 12:46:31 -0500

    Changes on Clients and Providers

---
+++ src/app/proveedores-list/proveedores-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proveedores-list',
+  imports: [],
+  templateUrl: './proveedores-list.component.html',
+  styleUrl: './proveedores-list.component.css'
+})
+export class ProveedoresListComponent {
+
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -17,23 +17,20 @@ export class ProductService {
 
   constructor(private http: HttpClient) {}
 
-  // Método para obtener los productos
+  
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl);
   }
 
-  // Método para eliminar un producto
   deleteProduct(id: string): Observable<void> {
     const url = `${this.apiUrl}/${id}`;
     return this.http.delete<void>(url);
   }
 
-  // Método para crear un nuevo producto
   createProduct(product: Product): Observable<Product> {
     return this.http.post<Product>(this.apiUrl, product);
   }
-
-  // Método para actualizar un producto
+  
   updateProduct(id: string, product: Product): Observable<Product> {
     const url = `${this.apiUrl}/${id}`;
     return this.http.put<Product>(url, product);
